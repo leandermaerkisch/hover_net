22c92a78c2d2692050603cd7f39a0382dfe532c7
leandermaerkisch <leander.maerkisch@floy.com>
2024-07-28 11:04:13 +0100

    Expand requirements

--- setup.py
+++ setup.py
@@ -1,25 +1,37 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="hover_net",
-    version="0.0.1",
+    name="hover-net",
+    version="0.0.4",
+    author="Leander Maerkisch",
+    author_email="l.maerkisch@gmail.com",
+    description="HoVer-Net implementation fork",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leandermaerkisch/hover_net",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "numpy",
         "opencv-python",
         "torch",
         "torchvision",
+        "matplotlib",
+        "pandas",
     ],
-    author="Leander Maerkisch",
-    author_email="l.maerkisch@gmail.com",
-    description="HoVer-Net implementation fork",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/leandermaerkisch/hover_net",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "hover-net-infer=hover_net.run_infer:main",
+            "hover-net-train=hover_net.run_train:main",
+        ],
+    },
 )
