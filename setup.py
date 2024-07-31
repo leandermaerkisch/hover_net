5a4edec2bb4b3f5fa40dcd1ed4dfe5c80f1dc311
leandermaerkisch <leander.maerkisch@floy.com>
2024-07-31 23:30:34 +0100

    Upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hover-net",
-    version="0.0.7",
+    version="0.0.8",
     author="Leander Maerkisch",
     author_email="l.maerkisch@gmail.com",
     description="HoVer-Net implementation fork",
